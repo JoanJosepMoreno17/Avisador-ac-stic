044f8628c16cbac6217b4417399d574f8119434e
JoanJosepMoreno17 <joan.moreno.29@lacetania.cat>
2018-05-11 11:11:18 +0200

    Activitats proposades

---
+++ Avisador_acustic_activitat_1/Avisador_acustic_activitat_1.ino
@@ -0,0 +1,26 @@
+/**********************************************************************************
+**                                                                               **
+**                              Avisador Acústic                                 **
+**                                                                               **
+** Joan Josep Moreno                                                             **
+**********************************************************************************/
+//********** Includes *************************************************************
+//********** Variables ************************************************************
+const int xiulet = 9;        // donar nom al pin 9 de l’Arduino
+const int ldr0 = A0;         // donar nom al pin A0 de l’Arduino
+int valLdr0;                 // guardar valor del ldr  
+int freq;                    // freqüència del xiulet
+//********** Setup ****************************************************************
+void setup()
+{
+ pinMode(xiulet, OUTPUT);               // definir el pin 9 com una sortida
+}
+//********** Loop *****************************************************************
+void loop()
+{
+ valLdr0 = analogRead(ldr0);            // llegir valor ldr
+ freq = map(freq, 0, 1023, 2000, 0);   // mapejar valor de 0-1023 a 2000-10
+ tone(xiulet, freq, 100);              // xiulet de durada 1s
+ delay(100+100);                           // esperar xiulet + silenci
+}
+//********** Funcions *************************************************************
